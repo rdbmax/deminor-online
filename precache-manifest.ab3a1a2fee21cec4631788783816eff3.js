c2b0c71db6d841359fcf9f0651d919c74f642f9a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-01 11:23:25 +0000

    Deploy rdbmax/deminor-online to github.com/rdbmax/deminor-online.git:gh-pages

---
+++ precache-manifest.ab3a1a2fee21cec4631788783816eff3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "64a3f009d601f567b467a0fadabfb820",
+    "url": "/deminor-online/static/media/md-flame.64a3f009.svg"
+  },
+  {
+    "revision": "5b62c93f19692d0ef6ce1c1d1638e935",
+    "url": "/deminor-online/static/media/ios-flag.5b62c93f.svg"
+  },
+  {
+    "revision": "c46586e2414ea2b15604",
+    "url": "/deminor-online/static/js/runtime~main.c46586e2.js"
+  },
+  {
+    "revision": "296fbd9cec2023b3c792",
+    "url": "/deminor-online/static/js/main.296fbd9c.chunk.js"
+  },
+  {
+    "revision": "90d716f466a52ed2fe08",
+    "url": "/deminor-online/static/js/1.90d716f4.chunk.js"
+  },
+  {
+    "revision": "296fbd9cec2023b3c792",
+    "url": "/deminor-online/static/css/main.ce101f01.chunk.css"
+  },
+  {
+    "revision": "a2722fac46ab43c812a01a8c8a5bdd3e",
+    "url": "/deminor-online/index.html"
+  }
+];
