25eac050262046869b227c3bff337fca9c269f07
Max Rdb <rdbmax@gmail.com>
2020-01-02 12:27:13 +0100

    Updates

---
+++ precache-manifest.72ea5e7907dd8885e92f36a1f42ea9f5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0949d8e0f991437991f1f172d7b34771",
+    "url": "/deminor-online/index.html"
+  },
+  {
+    "revision": "b97c8c87f10e923d06dc",
+    "url": "/deminor-online/static/css/main.0cd47cbc.chunk.css"
+  },
+  {
+    "revision": "f024847fd31dd8e34717",
+    "url": "/deminor-online/static/js/2.31bd876b.chunk.js"
+  },
+  {
+    "revision": "43611ca47bc87426c262f014dde99c44",
+    "url": "/deminor-online/static/js/2.31bd876b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b97c8c87f10e923d06dc",
+    "url": "/deminor-online/static/js/main.cea9f12a.chunk.js"
+  },
+  {
+    "revision": "5ea059f505d9abed6c8d",
+    "url": "/deminor-online/static/js/runtime-main.58f1ed41.js"
+  },
+  {
+    "revision": "5b62c93f19692d0ef6ce1c1d1638e935",
+    "url": "/deminor-online/static/media/ios-flag.5b62c93f.svg"
+  },
+  {
+    "revision": "64a3f009d601f567b467a0fadabfb820",
+    "url": "/deminor-online/static/media/md-flame.64a3f009.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/deminor-online/precache-manifest.ab3a1a2fee21cec4631788783816eff3.js"
+  "/deminor-online/precache-manifest.72ea5e7907dd8885e92f36a1f42ea9f5.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/deminor-online/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/deminor-online/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
